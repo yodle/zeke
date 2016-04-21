88aa0607274e54096707a379404c1da116ff41d7
Toni Ma <tma@yodle.com>
2016-04-21 19:17:00 -0400

    updating delete to delete all versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='zeke',
     version='0.2.0',
     author='Yodle',
-    author_email='noreply@yodle.com',
+    author_email='mdrago@yodle.com',
     url='https://git.yodle.com/projects/TOOL/repos/zeke/browse',
     description='Mess around with zookeeper',
     packages=['zeke'],
